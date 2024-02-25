6463ba7da990f9832156e0020b4a00989e9180fe
Supun Mahendra <supunmahendra@gmail.com>
2024-02-25 15:35:02 +0100

    JS sync

---
+++ index.js
@@ -0,0 +1,38 @@
+getProducts =(resourceURL, callback) =>{
+
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', ()=>{
+        if(request.status ===200 && request.readyState === 4){
+            // console.log(request.responseText);
+            callback(request.responseText, undefined);
+
+        }else if(request.readyState === 4){
+            callback(undefined, 'Can not Execute the request!')
+        }
+    })
+
+    // request.open('GET', 'https://fakestoreapi.com/products');
+    request.open('GET', resourceURL);
+
+    request.send();
+}
+
+
+
+
+const nowGetSometing = () =>{
+    return new Promise ((resolve,reject)=>{
+        //resolve('i got somting')
+        reject ('I did not get')
+
+    });
+
+}
+
+nowGetSometing().then((data)=>{
+    console.log("what our promise return:", data)
+}).catch((error)=>{
+    console.log(error)
+})
+
